4621c7169d11088ced4e6b1b8b4bc1e992197cc8
Collins Kiaritha <thuocollins020@gmail.com>
2025-11-26 05:22:35 +0300

    implemented the breadth traversal

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,18 @@
 class Tree:
-  def __init__(self, root = None):
+  def __init__(self, root=None):
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    if not self.root:
+      return None
+    
+    queue = [self.root]
+
+    while queue:
+      node = queue.pop(0)
+      if node.get('id') == id:
+        return node
+      # Add children to the end to maintain left-to-right order
+      queue.extend(node.get('children', []))
+
+    return None
